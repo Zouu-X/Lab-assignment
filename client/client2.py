5cf341eace78af9e42fae62b5c6e8612aafe917b
Zoe <380933593@qq.com>
2024-10-13 16:35:44 +0200

    exec-latency added

--- client/client2.py
+++ client/client2.py
@@ -1,12 +1,19 @@
 import rpyc
+import time
 
 def main():
     # 连接到负载均衡器
     conn = rpyc.connect("load-balancer", 18888)
     word_to_count = "sure"
     text_to_find = "Wonka.txt"
+
+    start_time = time.time()
+
     result = conn.root.get_word_count(word_to_count, text_to_find)
+    end_time = time.time()
+    exec_latency = end_time - start_time
     print(f"The word '{word_to_count}' appears {result} times in {text_to_find}")
+    print(f"Execution latency: {exec_latency:.6f} seconds")
     conn.close()
 
 if __name__ == "__main__":
