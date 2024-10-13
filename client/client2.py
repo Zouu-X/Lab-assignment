7adf23481d6df8f006e3fb27f57326b879b08855
Zoe <380933593@qq.com>
2024-10-13 09:17:17 +0200

    client2 added

--- client/client2.py
+++ client/client2.py
@@ -3,8 +3,8 @@ import rpyc
 def main():
     # 连接到负载均衡器
     conn = rpyc.connect("load-balancer", 18888)
-    word_to_count = "busy"
-    text_to_find = "spiderman.txt"
+    word_to_count = "sure"
+    text_to_find = "Wonka.txt"
     result = conn.root.get_word_count(word_to_count, text_to_find)
     print(f"The word '{word_to_count}' appears {result} times in {text_to_find}")
     conn.close()
