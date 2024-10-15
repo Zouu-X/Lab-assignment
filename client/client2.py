60a028d5191e7d9eeb94f8471955d1fd07f810dc
Zoe <380933593@qq.com>
2024-10-15 22:21:27 +0200

    change LB

--- client/client2.py
+++ client/client2.py
@@ -7,11 +7,17 @@ def main():
     word_to_count = "sure"
     text_to_find = "Wonka.txt"
 
+    # 开始时间戳
     start_time = time.time()
 
+    # 发送请求等响应
     result = conn.root.get_word_count(word_to_count, text_to_find)
+
+    # 结束时间戳
     end_time = time.time()
+
     exec_latency = end_time - start_time
+
     print(f"The word '{word_to_count}' appears {result} times in {text_to_find}")
     print(f"Execution latency: {exec_latency:.6f} seconds")
     conn.close()
