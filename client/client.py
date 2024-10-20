ec72c9b68942e88d22388fc7573e880b24111e38
Zoe <380933593@qq.com>
2024-10-20 23:36:00 +0200

    fix

--- client/client.py
+++ client/client.py
@@ -1,25 +1,39 @@
 import rpyc
 import time
+import csv
+
 def main():
     # 连接到负载均衡器
+
     conn = rpyc.connect("load-balancer", 18888)
+    latencies = []
     word_to_count = "hey"
     text_to_find = "Wonka.txt"
+    for _ in range(50):
 
-    # 开始时间戳
-    start_time = time.time()
+        # 开始时间戳
+        start_time = time.time()
 
-    # 发送请求等响应
-    result = conn.root.get_word_count(word_to_count, text_to_find)
+        # 发送请求等响应
+        result = conn.root.get_word_count(word_to_count, text_to_find)
 
-    # 结束时间戳
-    end_time = time.time()
+        # 结束时间戳
+        end_time = time.time()
 
-    exec_latency = end_time - start_time
+        exec_latency = end_time - start_time
+        latencies.append(exec_latency)
 
-    print(f"The word '{word_to_count}' appears {result} times in {text_to_find}")
-    print(f"Execution latency: {exec_latency:.6f} seconds")
+        print(f"The word '{word_to_count}' appears {result} times in {text_to_find}")
+        print(f"Execution latency: {exec_latency:.6f} seconds")
     conn.close()
+
+    # 保存exec latency
+    with open('/app/exec-latency/latency1.csv', mode='w', newline='') as file:
+        writer = csv.writer(file)
+        if file.tell() == 0:
+            writer.writerow(["Exection latency (s)"])
+        for latency in latencies:
+            writer.writerow([latency])
 
 if __name__ == "__main__":
     main()
--- client/client2.py
+++ client/client2.py
@@ -1,26 +1,38 @@
+import csv
+
 import rpyc
 import time
 
 def main():
+    latencies = []
     # 连接到负载均衡器
     conn = rpyc.connect("load-balancer", 18888)
     word_to_count = "sure"
     text_to_find = "Wonka.txt"
+    for _ in range(50):
 
-    # 开始时间戳
-    start_time = time.time()
+        # 开始时间戳
+        start_time = time.time()
 
-    # 发送请求等响应
-    result = conn.root.get_word_count(word_to_count, text_to_find)
+        # 发送请求等响应
+        result = conn.root.get_word_count(word_to_count, text_to_find)
 
-    # 结束时间戳
-    end_time = time.time()
+        # 结束时间戳
+        end_time = time.time()
 
-    exec_latency = end_time - start_time
+        exec_latency = end_time - start_time
+        latencies.append(exec_latency)
 
-    print(f"The word '{word_to_count}' appears {result} times in {text_to_find}")
-    print(f"Execution latency: {exec_latency:.6f} seconds")
+        print(f"The word '{word_to_count}' appears {result} times in {text_to_find}")
+        print(f"Execution latency: {exec_latency:.6f} seconds")
     conn.close()
+
+    with open('/app/exec-latency/latency2.csv', mode='w', newline='') as file:
+        writer = csv.writer(file)
+        if file.tell() == 0:
+            writer.writerow(["Exection latency (s)"])
+        for latency in latencies:
+            writer.writerow([latency])
 
 if __name__ == "__main__":
     main()
